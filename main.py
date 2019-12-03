8b5a83230a631f2b600246dae64a6c4583a20e4d
nightsea987 <shafigullina20031015@gmail.com>
2019-12-03 11:06:04 +0500

    added new file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, \
+    QAbstractItemView
 
 
 class MyWidget(QWidget):
@@ -9,6 +10,10 @@ class MyWidget(QWidget):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.con = sqlite3.connect("coffee.sqlite")
+        self.func = self.load_info
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.pushButton.clicked.connect(self.open_add_widget)
+        self.tableWidget.itemClicked.connect(self.open_edit_widget)
         self.load_info()
 
     def load_info(self):
@@ -20,7 +25,75 @@ class MyWidget(QWidget):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
         self.tableWidget.resizeColumnsToContents()
 
+    def open_edit_widget(self):
+        self.edit_info = EditCoffee(self.func, self.tableWidget.item(
+            self.tableWidget.currentRow(), 0).text())
+        self.edit_info.show()
 
+    def open_add_widget(self):
+        self.new_info = AddCoffee(self.func)
+        self.new_info.show()
+
+class AddCoffee(QWidget):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.add_info)
+
+    def add_info(self):
+        con = sqlite3.connect("coffee.sqlite")
+        cur = con.cursor()
+        cur.execute(f"""INSERT INTO coffee_info(variety, roast, beanOrGround,
+        taste, price, volume) VALUES('{self.lineEdit.text()}',
+        '{self.lineEdit_2.text()}', '{self.lineEdit_3.text()}',
+        '{self.lineEdit_4.text()}', {int(self.lineEdit_5.text())},
+        {int(self.lineEdit_6.text())})""")
+
+        con.commit()
+        con.close()
+        self.func()
+        self.close()
+
+
+class EditCoffee(QWidget):
+    def __init__(self, func, id=None):
+        super().__init__()
+        self.func = func
+        self.id = int(id)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.load_cells_info()
+        self.pushButton.clicked.connect(self.edit_info)
+
+    def load_cells_info(self):
+        self.con = sqlite3.connect("coffee.sqlite")
+        cur = self.con.cursor()
+        result = cur.execute(f"""SELECT * FROM coffee_info 
+        WHERE id = {self.id}""").fetchone()
+        self.con.commit()
+        self.con.close()
+
+        self.lineEdit.setText(str(result[1]))
+        self.lineEdit_2.setText(str(result[2]))
+        self.lineEdit_3.setText(str(result[3]))
+        self.lineEdit_4.setText(str(result[4]))
+        self.lineEdit_5.setText(str(result[5]))
+        self.lineEdit_6.setText(str(result[6]))
+
+    def edit_info(self):
+        con = sqlite3.connect("coffee.sqlite")
+        cur = con.cursor()
+        cur.execute(f"""UPDATE coffee_info SET variety = 
+        '{self.lineEdit.text()}', roast = '{self.lineEdit_2.text()}', 
+        beanOrGround = '{self.lineEdit_3.text()}',
+        taste = '{self.lineEdit_4.text()}', 
+        price = {int(self.lineEdit_5.text())},
+        volume = {int(self.lineEdit_6.text())} WHERE id = {self.id}""")
+
+        con.commit()
+        con.close()
+        self.func()
+        self.close()
 
 
 if __name__ == '__main__':
